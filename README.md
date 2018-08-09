1eebe2c079fdbd51e216d8b904e4d60ec599c569
zhaozj89 <zzhaoao@connect.ust.hk>
2018-08-09 20:18:56 +0800

    Add all packages

---
+++ README.md
@@ -0,0 +1,10 @@
+Prepare data for GAN training
+========
+
+## How to use ##
+
+* add all packages to Matlab path
+
+* first run `edge` to the model
+
+* run
--- batch.m
+++ batch.m
@@ -1,29 +1,22 @@
 %% set up
 source_path = '/Users/zzj/Documents/AR/Inshop/Img/';
+output_path = '/Users/zzj/Documents/AR/design-with-ml/output/';
 
 X=importdata('list_landmarks_inshop.txt','%s');
 
 len = size(X);
 
-%% load edge model (do not need it everytime)
-% % set opts for training (see edgesTrain.m)
-% opts=edgesTrain();                % default options (good settings)
-% opts.modelDir='edges/models/';          % model will be in models/forest
-% opts.modelFnm='edges/modelBsds';        % model name
-% opts.nPos=5e5; opts.nNeg=5e5;     % decrease to speedup training
-% opts.useParfor=0;                 % parallelize if sufficient memory
-% 
-% % train edge detector (~20m/8Gb per tree, proportional to nPos/nNeg)
-% tic, model=edgesTrain(opts); toc; % will load model if already trained
-% 
-% % set detection parameters (can set after training)
-% model.opts.multiscale=0;          % for top accuracy set multiscale=1
-% model.opts.sharpen=2;             % for top speed set sharpen=0
-% model.opts.nTreesEval=4;          % for top speed set nTreesEval=1
-% model.opts.nThreads=4;            % max number threads for evaluation
-% model.opts.nms=0;                 % set to true to enable nms
+% configuration of fashion cut
+options = config();
+
+% configuration for guided filter
+r = 60;
+eps = 10^-6;
+
+%% load edge model first
 
 %% Here I consider upper body image && all poses && all key points visible
+name_counter = 0;
 for idx = 1:len,
    input = strtrim(X{idx});
    chunck = strsplit(input, ' ');
@@ -35,6 +28,8 @@ for idx = 1:len,
        counter = 0;
        x_locs = [];
        y_locs = [];
+       landmark_for_crf = [];
+       visibility_for_crf = [];
        for kidx_out = 0:5,
            kidx_in = 4 + kidx_out*3;
            visibility = str2num(chunck{kidx_in});
@@ -43,26 +38,51 @@ for idx = 1:len,
            if visibility==0,
                counter=counter+1;
            end
+           landmark_for_crf = [landmark_for_crf, str2num(chunck{kidx_in+1}), str2num(chunck{kidx_in+2})];
+           visibility_for_crf = [visibility_for_crf, visibility];
        end
        if counter==6,
            rgb = imread([source_path, image_name]);
+           [h, w, c] = size(rgb);
+           
+           % segment rgb
+           for i=0:5,
+               landmark_for_crf(2*i+1) = landmark_for_crf(2*i+1)/w - 0.5;
+               landmark_for_crf(2*i+2) = landmark_for_crf(2*i+2)/h - 0.5;
+           end
+           
+           mask = FashionParsing(rgb, landmark_for_crf, visibility_for_crf, options);
+%            vmap = alphamask(rgb, mask, options);
+           mask = guidedfilter_color(im2double(rgb), mask, r, eps);
+           mask = repmat(mask, [1,1,3]);
+           white = im2double(ones(h,w));
+           white = repmat(white,[1,1,3]);
+           rgb = im2double(rgb);
+           rgb(:,:,1) = rgb(:,:,1).*mask(:,:,1) + white(:,:,1).*(1-mask(:,:,1));
+           rgb(:,:,2) = rgb(:,:,2).*mask(:,:,2) + white(:,:,2).*(1-mask(:,:,2));
+           rgb(:,:,3) = rgb(:,:,3).*mask(:,:,3) + white(:,:,3).*(1-mask(:,:,3));
+           rgb = im2uint8(rgb);
+%            imshow(rgb);
+%            break;
            
            % pose
-           [h, w, c] = size(rgb);
            pose = zeros(h, w);
            for i=1:6,
                pose(y_locs(i), x_locs(i)) = 255;
            end
-           se = offsetstrel('ball',5,5);
+           se = offsetstrel('ball',10,10);
            pose = imdilate(pose, se);
-           pose = cat(3, pose, pose, pose);
+           pose = repmat(pose, [1,1,3]);
 
-           res = [rgb, pose];
-           imshow(res);
-%            edge = edgesDetect(rgb, model);
-%            edge = im2uint8(1-edge);
-            break;
+           edge = edgesDetect(rgb, model);
+           edge = im2uint8(1-edge);
+           edge = repmat(edge, [1,1,3]);
+
+           res = [edge, pose, rgb];
            
+           output_name = sprintf('%04d.jpg', name_counter);
+           name_counter = name_counter+1;
+           imwrite(res, [output_path,output_name]);
        end
    end
 end
