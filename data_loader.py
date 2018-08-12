10fe1e0006192a9d7e243ca3a08bda1ef074be73
zhaozj89 <zzhaoao@connect.ust.hk>
2018-08-12 15:41:39 +0800

    Add test code

--- data_loader.py
+++ data_loader.py
@@ -10,6 +10,49 @@ class DataLoader():
     def __init__(self, img_size=(128, 128)):
         self.img_size = img_size
 
+    def load_data_test(self, batch_size=1, is_testing=False):
+        data_type = "train" if not is_testing else "test"
+        path = glob('../output/%s/*' % (data_type))
+
+        # batch_images = np.random.choice(path, size=batch_size)
+        batch_images = ['../output/train/00000200.jpg', '../output/train/00001001.jpg']
+
+        imgs_A = []
+        imgs_B = []
+        imgs_C = []
+        for img_path in batch_images:
+            img = self.imread(img_path)
+
+            h, w, _ = img.shape
+            _w = int(w/3)
+            img_A, img_B, img_C = img[:, :_w, :], img[:, _w:_w*2, :], img[:, _w*2:, :]
+
+            img_A = scipy.misc.imresize(img_A, self.img_size)
+            img_B = scipy.misc.imresize(img_B, self.img_size)
+            img_C = scipy.misc.imresize(img_C, self.img_size)
+
+            # print(img_A.shape)
+            ret, img_A = cv2.threshold(img_A, threshold, 255, cv2.THRESH_BINARY)
+            # print(img_A.shape)
+            # cv2.imshow('test', img_A)
+            # cv2.waitKey(0)
+
+            # If training => do random flip
+            # if not is_testing and np.random.random() < 0.5:
+            #     img_A = np.fliplr(img_A)
+            #     img_B = np.fliplr(img_B)
+            #     img_C = np.fliplr(img_C)
+
+            imgs_A.append(img_A)
+            imgs_B.append(img_B)
+            imgs_C.append(img_C)
+
+        imgs_A = np.array(imgs_A)/127.5 - 1.
+        imgs_B = np.array(imgs_B)/127.5 - 1.
+        imgs_C = np.array(imgs_C)/127.5 - 1.
+
+        return imgs_A, imgs_B, imgs_C
+
     def load_data(self, batch_size=1, is_testing=False):
         data_type = "train" if not is_testing else "test"
         path = glob('../output/%s/*' % (data_type))
--- test.py
+++ test.py
@@ -1,19 +1,49 @@
 from keras.models import load_model
 import numpy as np
 import cv2
+from data_loader import *
 
-im = cv2.imread('./datasets/facades/test/1.jpg', 1)
-input = im[:,0:256,:]
-input = input[np.newaxis,...]
+my_data_loader = DataLoader(img_size=(256, 256))
 
+# im = cv2.imread('./datasets/facades/test/1.jpg', 1)
+# input = im[:,0:256,:]
+# input = input[np.newaxis,...]
 # print(input.shape)
 
-model = load_model('./saved_model/epoch-1.h5')
+# step1. load data
+imgs_A, imgs_B, imgs_C = my_data_loader.load_data_test(batch_size=2, is_testing=False)
 
-output = model.predict(input)
+img_A = imgs_A[1,:,:,:] #(imgs_A[0,:,:,:] + imgs_A[1,:,:,:])*0.5
+img_B = imgs_A[0,:,:,:] #(imgs_B[0,:,:,:] + imgs_B[1,:,:,:])
+img_C = imgs_C[1,:,:,:]
 
-# print(type(output))
-# print(output.shape)
+img_A = img_A[np.newaxis,...]
+img_B = img_B[np.newaxis,...]
+img_C = img_C[np.newaxis,...]
 
-output = np.squeeze((output * 127.5 + 127.5).astype(np.uint8))
+# step2. load model
+model1 = load_model('../results/2stage/stage1-epoch-150.h5')
+model2 = load_model('../results/2stage/stage2-epoch-150.h5')
+
+# step3. predict results
+fake_A = model1.predict(img_B)
+sketch = np.concatenate([img_B, img_A], axis=3)
+fake_C = model2.predict(sketch)
+
+result = np.concatenate([img_B, fake_A, img_A, fake_C, img_C])
+# print(result[1].shape)
+
+result = 0.5 * result + 0.5
+
+titles = ['landmark', 'generated sketch', 'sketch', 'generated', 'original']
+fig, axs = plt.subplots(1, 5)
+for i in range(5):
+    axs[i].imshow(result[i])
+    axs[i].set_title(titles[i])
+    axs[i].axis('off')
+fig.savefig("test.png")
+plt.close()
+
+# print(fake_C.shape)
+output = np.squeeze((fake_A * 127.5 + 127.5).astype(np.uint8))
 cv2.imwrite('test.jpg', output)
