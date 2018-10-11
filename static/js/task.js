c34ff3041041117af42efb85ea279e7af29c8e4a
zhaozj89 <zzhaoao@connect.ust.hk>
2018-10-11 10:33:36 +0800

    first

--- static/js/task.js
+++ static/js/task.js
@@ -1,35 +1,35 @@
-function GetURLParameter(sParam) {
-  var sPageURL = window.location.search.substring(1);
-  var sURLVariables = sPageURL.split('&');
-  for (var i = 0; i < sURLVariables.length; i++) {
-    var sParameterName = sURLVariables[i].split('=');
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1];
-    }
-  }
-  return null;
-}
+// function GetURLParameter(sParam) {
+//   var sPageURL = window.location.search.substring(1);
+//   var sURLVariables = sPageURL.split('&');
+//   for (var i = 0; i < sURLVariables.length; i++) {
+//     var sParameterName = sURLVariables[i].split('=');
+//     if (sParameterName[0] === sParam) {
+//       return sParameterName[1];
+//     }
+//   }
+//   return null;
+// }
 
-var DEMO = GetURLParameter('demo');
+// var DEMO = GetURLParameter('demo');
 // console.log(DEMO);
 
-var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
+// var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 
-var pages = [
-    "instructions/instruct-1.html",
-    "instructions/instruct-ready.html",
-    "app.html",
-    "postquestionnaire.html"
-];
+// var pages = [
+//     "instructions/instruct-1.html",
+//     "instructions/instruct-ready.html",
+//     "app.html",
+//     "postquestionnaire.html"
+// ];
 
-psiTurk.preloadPages(pages);
+// psiTurk.preloadPages(pages);
 
-var instructionPages = [
-    "instructions/instruct-1.html",
-    "instructions/instruct-ready.html"
-];
+// var instructionPages = [
+//     "instructions/instruct-1.html",
+//     "instructions/instruct-ready.html"
+// ];
 
-var postquestionnairePage = "postquestionnaire.html";
+// var postquestionnairePage = "postquestionnaire.html";
 
 function ImageURI2DOM(uri) {
   var image = document.createElement("img");
@@ -69,36 +69,36 @@ var GetNeededRecordingData = function(){
 }
 
 var MyExperiment = function() {
-    psiTurk.showPage('app.html');
+    // psiTurk.showPage('app.html');
 
-    var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
+    // var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
 
-    prompt_resubmit = function() {
-      document.body.innerHTML = error_message;
-      $("#resubmit").click(resubmit);
-    };
+    // prompt_resubmit = function() {
+    //   document.body.innerHTML = error_message;
+    //   $("#resubmit").click(resubmit);
+    // };
 
-    resubmit = function() {
-      document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
-      reprompt = setTimeout(prompt_resubmit, 10000);
+    // resubmit = function() {
+    //   document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
+    //   reprompt = setTimeout(prompt_resubmit, 10000);
 
-      psiTurk.saveData({
-        success: function() {
-            currentview = new Questionnaire();
-        },
-        error: prompt_resubmit
-      });
-    };
+    //   psiTurk.saveData({
+    //     success: function() {
+    //         currentview = new Questionnaire();
+    //     },
+    //     error: prompt_resubmit
+    //   });
+    // };
 
-    GLOBAL.start = new Date();
+    // GLOBAL.start = new Date();
 
     $(function() {
 
-        if(DEMO!=="true"){
+        // if(DEMO!=="true"){
             GLOBAL.intro = introJs();
             GLOBAL.intro.setOption('showProgress', true);
             GLOBAL.intro.start();
-        }
+        // }
 
         // layers
         setTimeout(async function() {
@@ -106,12 +106,13 @@ var MyExperiment = function() {
             GLOBAL.svg = MakeClothingLandmark('SVGCanvas');
 
             // load image
-            let idx = Math.floor(Math.random()*59);
-            if(idx<10) GLOBAL.filename='000'+idx+'.png';
-            else GLOBAL.filename='00'+idx+'.png';
-            $('#ImageCanvas').attr('src', '/static/assets/images/show/'+GLOBAL.filename);
+            // let idx = Math.floor(Math.random()*59);
+            // if(idx<10) GLOBAL.filename='000'+idx+'.png';
+            // else GLOBAL.filename='00'+idx+'.png';
+            // $('#ImageCanvas').attr('src', '/static/assets/images/show/'+GLOBAL.filename);
+            // $('#ImageCanvas').attr('src', '/static/assets/images/2.jpg');
 
-            // GLOBAL.camera = MakeCamera('VideoCanvas');
+            GLOBAL.camera = MakeCamera('VideoCanvas');
             $(window).trigger('resize');
         }, 100);
 
@@ -170,113 +171,113 @@ var MyExperiment = function() {
                 case 'help':
                     GLOBAL.intro.start();
                     break;
-                case 'exit':
-                    psiTurk.recordUnstructuredData('all_data', GetNeededRecordingData());
-                    psiTurk.saveData({
-                      success: function() {
-                          currentview = new PostQuestionnaire();
-                      },
-                      error: prompt_resubmit
-                    });
-                    break;
+                // case 'exit':
+                //     psiTurk.recordUnstructuredData('all_data', GetNeededRecordingData());
+                //     psiTurk.saveData({
+                //       success: function() {
+                //           currentview = new PostQuestionnaire();
+                //       },
+                //       error: prompt_resubmit
+                //     });
+                //     break;
             }
         });
     });
 };
 
-var PostQuestionnaire = function() {
-  psiTurk.showPage(postquestionnairePage);
+// var PostQuestionnaire = function() {
+//   psiTurk.showPage(postquestionnairePage);
 
-  $(document).ready(function() {
-    // load your iframe with a url specific to your participant
-    $('#iframe').attr('src', 'https://ust.az1.qualtrics.com/jfe/form/SV_3UByw6DWqCghViZ?UID=' + uniqueId);
+//   $(document).ready(function() {
+//     // load your iframe with a url specific to your participant
+//     $('#iframe').attr('src', 'https://ust.az1.qualtrics.com/jfe/form/SV_3UByw6DWqCghViZ?UID=' + uniqueId);
 
-    var handler = function(event) {
-      // normally there would be a security check here on event.origin (see the MDN link above), but meh.
-      if (event.data) {
-        if (typeof event.data === 'string') {
-          q_message_array = event.data.split('|');
-          if (q_message_array[0] == 'QualtricsEOS') {
-            psiTurk.recordTrialData({
-              'phase': 'postquestionnaire',
-              'status': 'back_from_qualtrics'
-            });
-            psiTurk.recordUnstructuredData('qualtrics_session_id', q_message_array[2]);
+//     var handler = function(event) {
+//       // normally there would be a security check here on event.origin (see the MDN link above), but meh.
+//       if (event.data) {
+//         if (typeof event.data === 'string') {
+//           q_message_array = event.data.split('|');
+//           if (q_message_array[0] == 'QualtricsEOS') {
+//             psiTurk.recordTrialData({
+//               'phase': 'postquestionnaire',
+//               'status': 'back_from_qualtrics'
+//             });
+//             psiTurk.recordUnstructuredData('qualtrics_session_id', q_message_array[2]);
 
-            window.removeEventListener('message', handler);
+//             window.removeEventListener('message', handler);
 
-            psiTurk.recordTrialData({
-              'phase': 'finish_all',
-              'status': 'finish_all'
-            });
-            psiTurk.recordUnstructuredData('user_id', uniqueId);
+//             psiTurk.recordTrialData({
+//               'phase': 'finish_all',
+//               'status': 'finish_all'
+//             });
+//             psiTurk.recordUnstructuredData('user_id', uniqueId);
 
-            var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
+//             var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
 
-            prompt_resubmit = function() {
-              document.body.innerHTML = error_message;
-              $("#resubmit").click(resubmit);
-            };
+//             prompt_resubmit = function() {
+//               document.body.innerHTML = error_message;
+//               $("#resubmit").click(resubmit);
+//             };
 
-            resubmit = function() {
-              document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
-              reprompt = setTimeout(prompt_resubmit, 10000);
+//             resubmit = function() {
+//               document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
+//               reprompt = setTimeout(prompt_resubmit, 10000);
 
-              psiTurk.saveData({
-                success: function() {
-                  clearInterval(reprompt);
-                  $('#iframe').hide();
-                  var text = 'Finished, your survey code is: ' + uniqueId + '. Please fill it out at AMT website to finished the expriment. Thanks for your work.'
-                  $("#uniquecode").text(text);
-                },
-                error: prompt_resubmit
-              });
-            };
+//               psiTurk.saveData({
+//                 success: function() {
+//                   clearInterval(reprompt);
+//                   $('#iframe').hide();
+//                   var text = 'Finished, your survey code is: ' + uniqueId + '. Please fill it out at AMT website to finished the expriment. Thanks for your work.'
+//                   $("#uniquecode").text(text);
+//                 },
+//                 error: prompt_resubmit
+//               });
+//             };
 
-            psiTurk.saveData({
-              success: function() {
-                $('#iframe').hide();
-                var text = 'Finished, your survey code is:' + uniqueId + '. Please fill it out at AMT website to finished the expriment. Thanks for your work.'
-                $("#uniquecode").text(text);
-              },
-              error: prompt_resubmit
-            });
-          }
-        }
-      }
-    }
+//             psiTurk.saveData({
+//               success: function() {
+//                 $('#iframe').hide();
+//                 var text = 'Finished, your survey code is:' + uniqueId + '. Please fill it out at AMT website to finished the expriment. Thanks for your work.'
+//                 $("#uniquecode").text(text);
+//               },
+//               error: prompt_resubmit
+//             });
+//           }
+//         }
+//       }
+//     }
 
-    // add the all-important message event listener
-    window.addEventListener('message', handler);
+//     // add the all-important message event listener
+//     window.addEventListener('message', handler);
 
-    // fullscreen
-    function set_full() {
-      $('#iframe').css({
-        position: 'absolute',
-        width: $(window).width(),
-        height: $(window).height()
-      });
-    }
+//     // fullscreen
+//     function set_full() {
+//       $('#iframe').css({
+//         position: 'absolute',
+//         width: $(window).width(),
+//         height: $(window).height()
+//       });
+//     }
 
-    $(window).resize(function() {
-      set_full();
-    });
+//     $(window).resize(function() {
+//       set_full();
+//     });
 
-    set_full();
-  });
-}
+//     set_full();
+//   });
+// }
 
-var currentview;
+// var currentview;
 
 $(window).load(function() {
-    if(DEMO==="true")
-        currentview = new MyExperiment();
-    else {
-        psiTurk.doInstructions(
-            instructionPages,
-            function() {
-                currentview = new MyExperiment();
-            }
-        );
-    }
+    // if(DEMO==="true")
+    MyExperiment();
+    // else {
+    //     psiTurk.doInstructions(
+    //         instructionPages,
+    //         function() {
+    //             currentview = new MyExperiment();
+    //         }
+    //     );
+    // }
 });
